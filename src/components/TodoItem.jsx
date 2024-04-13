c7777e387b9bdf60416da6ceb2f3570aba96dc97
vinzz34 <vinaygn29@gmail.com>
2024-04-13 12:55:45 +0530

    added word break for long strings

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -29,7 +29,7 @@ const TodoItem = ({index,id,text,completed,todos,setTodos}) => {
                     <div className="w-5 h-5 grid place-content-center bg-gradient-to-br from-top to-bottom rounded-full">
                         <input onChange={handleChecked} checked={completed} className={`w-4 h-4 box-content appearance-none outline-none bg-white dark:bg-dark-desatBlue ${completed ? "border-none" : "border-2"} hover:border-none border-light-lightGrayishBlue dark:border-dark-veryDarkGrayishBlue rounded-full cursor-pointer checked:bg-[url:theme('backgroundImage.check'),theme('backgroundImage.gradient-to-br')] checked:bg-no-repeat checked:bg-center checked:from-top checked:to-bottom`} type="checkbox" />
                     </div>
-                    <p className={`${completed ? "line-through" : ""} ${completed ? "dark:text-dark-darkGrayishBlue" : "dark:text-dark-lightGrayishBlue"} ${completed ? "text-light-lightGrayishBlue" : "text-light-darkGrayishBlue"}`}>{text}</p>
+                    <p className={`${completed ? "line-through" : ""} ${completed ? "dark:text-dark-darkGrayishBlue" : "dark:text-dark-lightGrayishBlue"} ${completed ? "text-light-lightGrayishBlue" : "text-light-darkGrayishBlue"} break-all`}>{text}</p>
                 </div>
                 <img onClick={() => deleteTodo()} className="w-[14px] sm:w-[18px] h-[14px] sm:h-[18px] sm:hidden sm:group-hover:block cursor-pointer" src={cross} alt="delete" />
             </div>
